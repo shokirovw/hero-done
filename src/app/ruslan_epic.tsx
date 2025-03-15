86333186334fbe1a7e2359fcf6d094968f6a7a5b
shokirovw <shokirowv@gmail.com>
2025-03-16 02:03:36 +0500

    Added some things

---
+++ src/app/ruslan_epic.tsx
@@ -0,0 +1,23 @@
+{/* <div className="col-span-2 row-span-1 relative flex flex-col bg-gradient-to-br from-[#553e69] via-25% to-70% via-[#6e5386] to-[#9e79b7]  border border-gray-200 rounded-lg p-3">
+                        <div className="flex-grow">
+                            <div className="flex flex-row items-center gap-x-3">
+                                <Image src={badge_img} alt="" className="w-9 h-auto" width={200}  />
+                                <div>
+                                    <p className="font-rwdevi text-[15px] -mt-0.5 text-white">Pres. Aleksandrov</p>
+                                    <p className="text-xs text-[#dfbef2]">r.aleksanrdov@newuu.uz</p>
+                                </div>
+                            </div>
+                        </div>
+                        <TbPinFilled className="absolute text-gray-300 top-2 right-2 w-3 h-auto" />
+                        <div className="w-full flex flex-row gap-x-3">
+                            <Link href={"/profile"} className="flex-[3] text-xs inline-flex items-center font-medium border-input shadow-sm h-8 rounded-md px-3 flex-row gap-x-2 bg-[#f9f0ff] hover:bg-white/80 justify-center">
+                                <p>Profile</p>
+                                <CgProfile />
+                            </Link>
+                            <div onClick={() => { setScanScreen({ ...scanScreen, open: true }) }} className="flex-[1] cursor-pointer text-[11px] items-center inline-flex font-medium border-input shadow-sm h-8 rounded-md px-3 flex-row gap-x-2 bg-[#553e69] text-white justify-center">
+                                <p>Scan</p>
+                                <LiaQrcodeSolid />
+                            </div>
+                            
+                        </div>
+                    </div> */}
